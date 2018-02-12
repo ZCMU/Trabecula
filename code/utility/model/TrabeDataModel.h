43f9e62d7c9344f0da40594dc69199209d9b58ad
YX <yxxinyuan@zju.edu.cn>
2018-02-12 15:43:07 +0800

    增加Model层的类

---
+++ code/utility/model/TrabeDataModel.h
@@ -0,0 +1,44 @@
+////////////////////////////////////////////////////////////////////////////////
+#pragma once
+////////////////////////////////////////////////////////////////////////////////
+
+#include <imp\ImageData.h>
+
+////////////////////////////////////////////////////////////////////////////////
+
+class TrabeDataModel : public Proxy_PropertyNotification<TrabeDataModel>
+{
+public:
+	TrabeDataModel() throw()
+	{
+	}
+	~TrabeDataModel() throw()
+	{
+	}
+
+	//properties
+	const ColorData& get_ColorData() const throw()
+	{
+		return m_cData;
+	}
+
+	//load
+	bool Load(const std::string& strFile)
+	{
+		ATL::CImage image;
+		if( FAILED(image.Load(A2T(strFile._str())) )
+			return false;
+		int iBPP = image.GetBPP();
+		if( iBPP != 8 && iBPP != 24 )
+			return false;
+		ImageDataHelper::ImageToColorData(image, m_cData);
+		//notify
+		Fire_OnPropertyChanged(std::string("color_data"));
+		return true;
+	}
+
+private:
+	ColorData m_cData;
+};
+
+////////////////////////////////////////////////////////////////////////////////
